5b2f37aa36e555c3617cdc5576d6c80c9a68580a
MikeTschudi <mtschudi@esri.com>
2014-04-14 19:02:50 -0700

    Standardized JSLint exceptions

--- js/library/widgets/print/printMap.js
+++ js/library/widgets/print/printMap.js
@@ -1,21 +1,20 @@
-﻿/*global dojo,define,document,window */
-/*jslint sloppy:true */
-/** @license
-| Version 10.2
-| Copyright 2013 Esri
-|
-| Licensed under the Apache License, Version 2.0 (the "License");
-| you may not use this file except in compliance with the License.
-| You may obtain a copy of the License at
-|
-|    http://www.apache.org/licenses/LICENSE-2.0
-|
-| Unless required by applicable law or agreed to in writing, software
-| distributed under the License is distributed on an "AS IS" BASIS,
-| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-| See the License for the specific language governing permissions and
-| limitations under the License.
-*/
+﻿/*global dojo,define */
+/*jslint browser:true,sloppy:true,nomen:true,unparam:true,plusplus:true */
+/*
+ | Copyright 2013 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
 //============================================================================================================================//
 define([
     "dojo/_base/declare",
@@ -24,7 +23,7 @@ define([
     "dojo/topic",
     "dojo/_base/lang",
     "dijit/_WidgetBase",
-    "esri/map",     
+    "esri/map",
     "dojo/text!./templates/print.html"
   ],
 function (declare, domConstruct, on, topic, lang, _WidgetBase, esriMap, printMap, Print) {
@@ -45,7 +44,7 @@ function (declare, domConstruct, on, topic, lang, _WidgetBase, esriMap, printMap
         * @class
         * @name widgets/printMap/printMap
         */
-        postCreate: function () {                      
+        postCreate: function () {
             this.domNode = domConstruct.create("div", { "title": this.title, "class": "esriCTImgPrint" }, null);
 
             this.own(on(this.domNode, "click", lang.hitch(this, function () {
